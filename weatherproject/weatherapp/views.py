d4926b78d0f1fea5cd1d63cc94c4bdcef708faa7
khasbin <khasbin@gmail.com>
2021-02-06 13:16:58 +0545

    url added

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, HttpResponse
+import requests
 # Create your views here.
 
 def home(request):
-    return render(request,'weatherapp/index.html')
+    url = "http://api.weatherapi.com/v1/current.json?key=ac457ea4fc2a4b4fa1b51740210602&q={}"
+
+    city = request.GET.get("cname")
+
+    r = requests.get(url.format(city)).json()
+    weather = {
+        'city':city,
+        'temperature':r['current']['temp_c'],
+        'condition':r['current']['condition']['text'],
+        'icon':r['current']['condition']['icon']
+    }
+    context = {'weather':weather}
+
+    return render(request,'weatherapp/index.html',context)
     
